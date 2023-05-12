f977eac354ce25f94c745e537698ed3108d20fdc
Luis <roberto_lugo95@yahoo.com>
2023-05-12 18:23:41 -0500

    fixed edit error

--- views/Edit.jsx
+++ views/Edit.jsx
@@ -2,22 +2,22 @@ const React = require("react");
 
 class Edit extends React.Component {
   render() {
-    const logs = this.props;
+    const { log } = this.props;
     return (
       <div>
         <h1>Edit Log Entry</h1>
-        <form action={`/logs/${logs._id}?_method=PUT`} method="POST">
+        <form action={`/logs/${log._id}?_method=PUT`} method="POST">
           <label>Title:</label>
-          <input type="text" name="title" defaultValue={logs.title} />
+          <input type="text" name="title" defaultValue={log.title} />
 
           <label>Entry:</label>
-          <input type="textarea" name="entry" defaultValue={logs.entry} />
+          <input type="textarea" name="entry" defaultValue={log.entry} />
 
           <label>Is the ship broken?</label>
           <input
             type="checkbox"
             name="shipIsBroken"
-            defaultChecked={logs.shipIsBroken}
+            defaultChecked={log.shipIsBroken}
           />
           <input type="submit" value="Update Log Entry" />
         </form>
